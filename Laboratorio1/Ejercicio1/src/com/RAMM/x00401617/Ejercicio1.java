9c19960f92d639775d970f17db4457621caf170c
ronmiranda <00401617@uca.edu.sv>
2020-03-17 16:29:22 -0600

    Funcion recursiva

--- Laboratorio1/Ejercicio1/src/com/RAMM/x00401617/Ejercicio1.java
+++ Laboratorio1/Ejercicio1/src/com/RAMM/x00401617/Ejercicio1.java
@@ -1,6 +1,6 @@
 package com.RAMM.x00401617;
 import java.util.Scanner;
-public class Main {
+public class Ejercicio1 {
 
     public static void main(String[] args) {
         // write your code here
