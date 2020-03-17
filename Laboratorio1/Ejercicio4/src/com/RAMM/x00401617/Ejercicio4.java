3234019d94af815f98174313f616e3aa6b349682
ronmiranda <00401617@uca.edu.sv>
2020-03-17 16:41:15 -0600

    Declaracion de variables

--- Laboratorio1/Ejercicio4/src/com/RAMM/x00401617/Ejercicio4.java
+++ Laboratorio1/Ejercicio4/src/com/RAMM/x00401617/Ejercicio4.java
@@ -1,8 +1,12 @@
 package com.RAMM.x00401617;
 
+import  java.util.Scanner;
+
 public class Ejercicio4 {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner sca=new Scanner(System.in);
+        String sTexto="";
     }
 }
