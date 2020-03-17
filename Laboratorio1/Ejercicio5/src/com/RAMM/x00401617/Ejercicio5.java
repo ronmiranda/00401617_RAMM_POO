43762ef52f292df8c362cd31c851d11bea886f96
ronmiranda <00401617@uca.edu.sv>
2020-03-17 16:47:49 -0600

    Declaracion de variables y entrada de datos por teclado

--- Laboratorio1/Ejercicio5/src/com/RAMM/x00401617/Ejercicio5.java
+++ Laboratorio1/Ejercicio5/src/com/RAMM/x00401617/Ejercicio5.java
@@ -1,8 +1,15 @@
 package com.RAMM.x00401617;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Ejercicio5 {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner sca=new Scanner(System.in);
+        ArrayList lista=new ArrayList();
+        // char a,e,i,o,u;
+        String cadena="";
+        System.out.println("Introduzca una frase");
+        cadena=sca.nextLine();
     }
 }
