fb4eee96f847d73674646f91a3b36e6f4dc3db0d
ronmiranda <00401617@uca.edu.sv>
2020-03-17 15:51:12 -0600

    Declaracion de variables

--- Laboratorio1/Ejercicio1/src/com/RAMM/x00401617/Main.java
+++ Laboratorio1/Ejercicio1/src/com/RAMM/x00401617/Main.java
@@ -1,8 +1,13 @@
 package com.RAMM.x00401617;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner sca=new Scanner(System.in);
+        int x=0,y=0,solucion;
+
+        System.out.print("Ingrese la cantidad de numeros a calcular:");
+        int n=sca.nextInt();
     }
 }
