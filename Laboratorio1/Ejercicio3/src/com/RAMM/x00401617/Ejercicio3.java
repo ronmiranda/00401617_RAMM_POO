7c9bcc7bcfb9bb3acfdea1f550dda274de97e105
ronmiranda <00401617@uca.edu.sv>
2020-03-17 16:37:25 -0600

    Llamada a la funcion recursiva

--- Laboratorio1/Ejercicio3/src/com/RAMM/x00401617/Ejercicio3.java
+++ Laboratorio1/Ejercicio3/src/com/RAMM/x00401617/Ejercicio3.java
@@ -11,5 +11,6 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         // write your code here
         Ejercicio3 recursivo=new Ejercicio3();
+        recursivo.imprimir(3);
     }
 }
