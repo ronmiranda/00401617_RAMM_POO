e41e2d1e08548965dd232486e1d2ef6b8bf47b17
ronmiranda <00401617@uca.edu.sv>
2020-03-17 16:11:26 -0600

    Declaracion de variables y lectura de datos por teclado

--- Laboratorio1/Ejercicio2/src/com/RAMM/x00401617/Main.java
+++ Laboratorio1/Ejercicio2/src/com/RAMM/x00401617/Main.java
@@ -1,8 +1,17 @@
 package com.RAMM.x00401617;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
     // write your code here
+        Scanner sca=new Scanner (System.in);
+
+        System.out.println("Introduce una frase");
+        String frase=sca.nextLine();
+        frase=frase.replaceAll("\\s+", "");
+        System.out.println(frase);
+        int ini=0;
+        int fin=frase.length()-1;
+        boolean palindromo=true;
 }
 }
