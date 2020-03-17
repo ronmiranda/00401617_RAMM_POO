eada5ba6b2c5a8e6c713a2951004be49b420fc6f
ronmiranda <00401617@uca.edu.sv>
2020-03-17 16:19:54 -0600

    Funcion recursiva

--- Laboratorio1/Ejercicio3/src/com/RAMM/x00401617/Main.java
+++ Laboratorio1/Ejercicio3/src/com/RAMM/x00401617/Main.java
@@ -1,7 +1,13 @@
 package com.RAMM.x00401617;
 
 public class Main {
+    public void imprimir(int n){
+        if(n>0){
+            imprimir(n-1);
+            System.out.print(n+",");
 
+        }
+    }
     public static void main(String[] args) {
         // write your code here
     }
