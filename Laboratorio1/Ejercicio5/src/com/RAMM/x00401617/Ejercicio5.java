d58e5eb1c0110a6bdce2e1e9fde43749a8e18daf
ronmiranda <00401617@uca.edu.sv>
2020-03-17 16:50:17 -0600

    for y while en el que se iran comparando letra por letra de la cadena de texto ingresa y almacenar las vocales en una lista

--- Laboratorio1/Ejercicio5/src/com/RAMM/x00401617/Ejercicio5.java
+++ Laboratorio1/Ejercicio5/src/com/RAMM/x00401617/Ejercicio5.java
@@ -11,5 +11,42 @@ public class Ejercicio5 {
         String cadena="";
         System.out.println("Introduzca una frase");
         cadena=sca.nextLine();
+
+        for(int x=0;x<cadena.length();x++){
+            switch (cadena.charAt(x)) {
+                case 'a':
+                    lista.add('a');
+                    break;
+                case 'e':
+                    lista.add('e');
+                    break;
+                case 'i':
+                    lista.add('i');
+                    break;
+                case 'o':
+                    lista.add('o');
+                    break;
+                case 'u':
+                    lista.add('u');
+                    break;
+                case 'A':
+                    lista.add('A');
+                    break;
+                case 'E':
+                    lista.add('E');
+                    break;
+                case 'I':
+                    lista.add('I');
+                    break;
+                case 'O':
+                    lista.add('O');
+                    break;
+                case 'U':
+                    lista.add('U');
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
